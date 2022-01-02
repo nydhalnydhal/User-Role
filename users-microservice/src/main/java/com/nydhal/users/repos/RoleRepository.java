31d4e1dcdd15c887dd123ae44b936ba483e36b0d
nydhalnydhal <bougarradhnidhal@gmail.com>
2022-01-02 18:57:00 +0100

    Initial Commit

---
+++ users-microservice/src/main/java/com/nydhal/users/repos/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.nydhal.users.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.nydhal.users.entities.Role;
+public interface RoleRepository extends JpaRepository<Role, Long> {
+Role findByRole(String role);
+}
---
+++ users-microservice/src/main/java/com/nydhal/users/repos/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nydhal.users.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.nydhal.users.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+ User findByUsername(String username);
+
+}
